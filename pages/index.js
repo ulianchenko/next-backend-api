975d1cbbd972c90437ef8ec6dce770086b5cf0bf
ulianchenko <ulianchenko@gmail.com>
2025-01-28 01:58:51 +0200

    frontend form was prepared

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInput = useRef();
+  const feedbackInput = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInput.current.value;
+    const enteredFeedback = feedbackInput.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInput} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInput}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
